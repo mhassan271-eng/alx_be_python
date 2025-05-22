ddee9183fd68c3050578d262635be619714b0580
ةاشسسشى <mohamed_hasan.kano@yahoo.com>
2025-05-22 13:52:33 +0300

    Initial commit: basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# File: basic_operations.py
+
+# تعريف المتغيرات
+number1 = 10
+number2 = 5
+
+# العمليات الحسابية
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# طباعة النتائج
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
+
