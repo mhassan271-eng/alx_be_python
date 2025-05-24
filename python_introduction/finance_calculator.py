308493d0836689865ff0b2936d09934c5376029a
ةاشسسشى <mohamed_hasan.kano@yahoo.com>
2025-05-24 12:16:59 +0300

    sheet modified

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,3 +1,4 @@
+
 # استلام البيانات من المستخدم
 income = float(input("Enter your monthly income: "))
 expenses = float(input("Enter your total monthly expenses: "))
@@ -6,10 +7,12 @@ expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = income - expenses
 
 # حساب الادخار السنوي مع الفايدة (5%)
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+Projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # طباعة النتائج
-print(f"Your monthly savings: {monthly_savings}")
-print(f"Your projected savings after one year with 5% interest: 
-{annual_savings}")
+print(f"Your monthly savings are: {monthly_savings}")
+print(f"projected savings after one year, with interest, is: 
+{Projected_Savings}")
+
+
 
