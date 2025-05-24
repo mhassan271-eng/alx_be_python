a6749330a3c0d202b00bce53f5889aa6ecc829ad
ةاشسسشى <mohamed_hasan.kano@yahoo.com>
2025-05-24 11:41:04 +0300

    sheet added

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# نطلب من المستخدم يدخل عمره الحالي
+current_age = int(input("How old are you? "))
+
+# نحسب العمر في سنة 2050 (بما إن 2050 - 2023 = 27)
+age_in_2050 = current_age + 27
+
+# نطبع النتيجة بالطريقة المطلوبة
+print(f"In 2050, you will be {age_in_2050} years old.")
